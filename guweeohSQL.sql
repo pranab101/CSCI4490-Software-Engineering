8b275fe4f051020121b0007a7751b943c1ae36b4
pranab101 <pranabpudasaini@yahoo.com>
2023-12-05 13:08:47 -0600

    Create guweeohSQL.sql

---
+++ guweeohSQL.sql
@@ -0,0 +1,16 @@
+DROP TABLE users; 
+
+CREATE TABLE users(
+username varchar(30),
+password varbinary(16)
+wins int(5);
+
+
+ALTER TABLE users
+	add constraint users_username_pk primary key(username);
+	
+	
+insert into users
+	values('team4', aes_encrypt('password', 'key'), 0);
+
+
