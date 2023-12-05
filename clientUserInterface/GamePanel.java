0e9c998b4017a2b052f2eaaf8ab61b71d9f739f3
Daniel Freeman <89107226+vtmndanny@users.noreply.github.com>
2023-12-04 18:54:32 -0600

    Update GamePanel.java

--- clientUserInterface/GamePanel.java
+++ clientUserInterface/GamePanel.java
@@ -4,48 +4,48 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 
-
 import clientCommunication.*;
 
-public class GamePanel extends JPanel 
-{
-	public GamePanel(GameControl gc)
-	{
+public class GamePanel extends JPanel {
+	public GamePanel(GameControl gc) {
 		JPanel north = new JPanel();
 		JPanel east = new JPanel();
-		JPanel south = new JPanel();
 		JPanel west = new JPanel();
 		JPanel center = new JPanel();
-		
+		JPanel container = new JPanel(new BorderLayout());
+
+		setBackground(new Color(255, 255, 255));
+
 		JLabel statusText = new JLabel("Score: [Wizard1Score] - [Wizard2Score]");
 		north.add(statusText);
-		
+
 		// West - Wizard 1 HP and Cards
 		JPanel westRowPanel = new JPanel(new GridLayout(0, 1));
-		west.add(westRowPanel);
-		
+
 		JLabel wizard1healthLabel = new JLabel("Wizard 1 - [wizard1health] HP");
 		westRowPanel.add(wizard1healthLabel);
 
 		JLabel wizard1card1 = new JLabel("");
-		wizard1card1.setIcon(new ImageIcon(GamePanel.class.getResource("./fire.png")));
+		wizard1card1.setIcon(new ImageIcon(GamePanel.class.getResource("/clientUserInterface/fire.png")));
 		westRowPanel.add(wizard1card1);
-		
+
 		JLabel wizard1card2 = new JLabel("");
-		wizard1card2.setIcon(new ImageIcon(GamePanel.class.getResource("./water.png")));
+		wizard1card2.setIcon(new ImageIcon(GamePanel.class.getResource("/clientUserInterface/water.png")));
 		westRowPanel.add(wizard1card2);
 
 		JLabel wizard1card3 = new JLabel("");
-		wizard1card3.setIcon(new ImageIcon(GamePanel.class.getResource("./electricity.png")));
+		wizard1card3.setIcon(new ImageIcon(GamePanel.class.getResource("/clientUserInterface/electricity.png")));
 		westRowPanel.add(wizard1card3);
 
+		west.add(westRowPanel);
+
 		// Center - Wizard Icon, Statuses, and Card Selections
-		JPanel centerGrid = new JPanel(new GridLayout(3, 1));
+		JPanel centerGrid = new JPanel(new GridLayout(4, 1));
 
 		JLabel wizardIcon = new JLabel("");
-		wizardIcon.setIcon(new ImageIcon(GamePanel.class.getResource("./wizardclear.png")));
+		wizardIcon.setIcon(new ImageIcon(GamePanel.class.getResource("/clientUserInterface/wizardclear.png")));
 		centerGrid.add(wizardIcon);
-		
+
 		JPanel cardChoicePanel = new JPanel(new GridLayout(2, 1));
 		cardChoicePanel.setSize(300, 50);
 
@@ -58,6 +58,7 @@ public class GamePanel extends JPanel
 
 		JPanel card2choicePanel = new JPanel();
 		card2choicePanel.setSize(300, 50);
+		setLayout(new BorderLayout(0, 0));
 
 		JLabel card2choice = new JLabel("Card 2:");
 		cardChoicePanel.add(card2choice);
@@ -76,42 +77,48 @@ public class GamePanel extends JPanel
 		statusBox.add(status);
 		centerGrid.add(statusBox);
 
+		// South - Play Hand / Play Again / Quit Game Buttons
+		JPanel buttons = new JPanel(new GridLayout(3, 0));
+		JButton playHand = new JButton("Play Hand");
+		buttons.add(playHand);
+
+		JButton playAgain = new JButton("Play Again");
+		playAgain.setVisible(false);
+		buttons.add(playAgain);
+
+		JButton quitGame = new JButton("Quit Game");
+		quitGame.setVisible(false);
+		buttons.add(quitGame);
+
+		centerGrid.add(buttons);
+
 		center.add(centerGrid);
 
 		// East - Wizard 2 HP and Cards
 		JPanel eastRowPanel = new JPanel(new GridLayout(0, 1));
-		east.add(eastRowPanel);
 
-		JLabel wizard2healthLabel = new JLabel("Wizard 1 - [wizard1health] HP");
+		JLabel wizard2healthLabel = new JLabel("Wizard 2 - [wizard2health] HP");
 		eastRowPanel.add(wizard2healthLabel);
 
 		JLabel wizard2card1 = new JLabel("");
-		wizard2card1.setIcon(new ImageIcon(GamePanel.class.getResource("./health.png")));
+		wizard2card1.setIcon(new ImageIcon(GamePanel.class.getResource("/clientUserInterface/health.png")));
 		eastRowPanel.add(wizard2card1);
 
 		JLabel wizard2card2 = new JLabel("");
-		wizard2card2.setIcon(new ImageIcon(GamePanel.class.getResource("./water.png")));
+		wizard2card2.setIcon(new ImageIcon(GamePanel.class.getResource("/clientUserInterface/water.png")));
 		eastRowPanel.add(wizard2card2);
 
 		JLabel wizard2card3 = new JLabel("");
-		wizard2card3.setIcon(new ImageIcon(GamePanel.class.getResource("./fire.png")));
+		wizard2card3.setIcon(new ImageIcon(GamePanel.class.getResource("/clientUserInterface/fire.png")));
 		eastRowPanel.add(wizard2card3);
 
-		// South - Play Hand / Play Again / Quit Game Buttons
-		JButton playHand = new JButton("Play Hand");
-		south.add(playHand);
+		east.add(eastRowPanel);
 
-		JButton playAgain = new JButton("Play Again");
-		south.add(playAgain);
+		container.add(north, BorderLayout.NORTH);
+		container.add(east, BorderLayout.EAST);
+		container.add(west, BorderLayout.WEST);
+		container.add(center, BorderLayout.CENTER);
 
-		JButton quitGame = new JButton("Quit Game");
-		south.add(quitGame);
-
-		this.add(north, BorderLayout.NORTH);
-		this.add(east, BorderLayout.EAST);
-		this.add(south, BorderLayout.SOUTH);
-		this.add(west, BorderLayout.WEST);
-		this.add(center, BorderLayout.CENTER);
+		this.add(container);
 	}
 }
-
